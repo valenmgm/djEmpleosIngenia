bae3e613cedc97f56dc273f935706b80a4da23df
valenmgm <vmgmjr@gmail.com>
2016-07-20 12:35:09 -0500

    cambios para form funcional hechos. Falta crear virtualenv

--- djEmpleosIngenia/settings.py
+++ djEmpleosIngenia/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #externals
+    "materializecssform",
     #MyApps
     'homeApp',
     'ingeniaApp',
